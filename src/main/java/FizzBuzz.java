59a8d037a9e717530ca794f7828fd9f557d402b4
JCPosso <44146884+JCPosso@users.noreply.github.com>
2026-02-27 02:31:10 +0000

    Refactor fizzbuzz method to simplify logic and improve readability

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,14 +1,12 @@
+/**
+ * Implementación juego FizzBuzz.
+ */
 public class FizzBuzz {
+    
     public static String fizzbuzz(int n) {
-        if (n % 15 == 0) {
-            return "FizzBuzz";
-        }
-        if (n % 3 == 0) {
-            return "Fizz";
-        }
-        if (n % 5 == 0) {
-            return "Buzz";
-        }
-        return String.valueOf(n);
+        String result = "";
+        if (n % 3 == 0) result += "Fizz";
+        if (n % 5 == 0) result += "Buzz";
+        return result.isEmpty() ? String.valueOf(n) : result;
     }
 }
