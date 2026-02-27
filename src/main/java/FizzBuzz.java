4b384560f5c7f0235ee79d53351ffb90642606b4
JCPosso <44146884+JCPosso@users.noreply.github.com>
2026-02-27 03:10:18 +0000

    Refactor FizzBuzz implementation for clarity and maintainability

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,10 +3,28 @@
  */
 public class FizzBuzz {
     
-    public static String fizzbuzz(int n) {
-        String result = "";
-        if (n % 3 == 0) result += "Fizz";
-        if (n % 5 == 0) result += "Buzz";
-        return result.isEmpty() ? String.valueOf(n) : result;
+    public static String fizzbuzz(int number) {
+        if (isFizzBuzz(number)) {
+            return "FizzBuzz";
+        }
+        if (isFizz(number)) {
+            return "Fizz";
+        }
+        if (isBuzz(number)) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
+
+    private static boolean isFizzBuzz(int number) {
+        return number % 15 == 0;
+    }
+
+    private static boolean isFizz(int number) {
+        return number % 3 == 0;
+    }
+
+    private static boolean isBuzz(int number) {
+        return number % 5 == 0;
     }
 }
